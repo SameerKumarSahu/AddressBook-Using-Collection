d70b278ea701708009fca0c0733f270337b6a554
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-05 22:39:27 +0530

    Edit the exit contact details using their name

--- AddressBook.java
+++ AddressBook.java
@@ -8,7 +8,7 @@ public class AddressBook {
 	static Scanner sc = new Scanner(System.in);
 
 	static ArrayList<ContactDetails> personLists = new ArrayList<ContactDetails>();
-
+	//Adding a new contact details
 	public static void addContact() {
 
 		System.out.println("Enter the First name");
@@ -39,6 +39,61 @@ public class AddressBook {
 		personLists.add(newPerson);
 		System.out.println(personLists);
 
+	}
+	//Edit the contact details
+	public static void editContact() {
+		System.out.println("Confirm your first name to edit details: ");
+		String confirm_name = sc.next();
+
+		for (int i = 0; i < personLists.size(); i++) {
+			if (personLists.get(i).getFirstName().equals(confirm_name)) {
+				System.out.println("Select form below to change: ");
+				System.out.println(
+						"\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Zip\n7.Mobile number\n8.Email");
+
+				int option = sc.nextInt();
+
+				switch (option) {
+				case 1:
+					System.out.println("Enter new First Name: ");
+					
+					personLists.get(i).setFirstName(sc.next());
+					break;
+				case 2:
+					System.out.println("Enter new Last name");
+					personLists.get(i).setLastName(sc.next());
+					break;
+				case 3:
+					System.out.println("Enter new Address");
+					personLists.get(i).setAddress(sc.next());
+					break;
+				case 4:
+					System.out.println("Enter new City");
+					personLists.get(i).setCity(sc.next());
+					break;
+				case 5:
+					System.out.println("Enter new State");
+					personLists.get(i).setState(sc.next());
+					break;
+				case 6:
+					System.out.println("Enter new Zip");
+					personLists.get(i).setZip(sc.nextLong());
+					break;
+				case 7:
+					System.out.println("Enter new Mobile number");
+					personLists.get(i).setPhoneNum(sc.nextLong());
+					break;
+				case 8:
+					System.out.println("Enter new E-mail");
+					personLists.get(i).setEmail(sc.next());
+					break;
+				}
+				System.out.println("Contact details Edit sucessufully..: ");
+				System.out.println(personLists);
+
+			} else
+				System.out.println("Enter valid First name");
+		}
 	}
 
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,11 +1,30 @@
 package com.Addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to AddressBook System...");
-		// Calling the addcontact method
-		AddressBook.addContact();
+		Scanner sc = new Scanner(System.in);
+		boolean flag = true;
+		while (flag) {
+
+			System.out.println("1.Adding Contact Details" + "\n2.Editing the Contact details" + "\n3.Press 0 for exit");
+			int option = sc.nextInt();
+			switch (option) {
+
+			case 1:
+				AddressBook.addContact();
+				break;
+			case 2:
+				AddressBook.editContact();
+				break;
+			default:
+				flag = false;
+				System.out.println("Thank You");
+			}
+
+		}
 	}
 
 }
